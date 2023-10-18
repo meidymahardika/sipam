b76249294d3fec7eb519e1696860c38c954d34b9
meidymahardika <meidymahardika300501@gmail.com>
2023-10-18 21:17:55 +0700

    add antd

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Row, Col, Result, Button } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sipam
-        </p>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Row style={{ margin: 200 }}>
+      <Col span={24}>
+        <Result
+          status="success"
+          title="Successfully Purchased Cloud Server ECS!"
+          subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
+          extra={[
+            <Button type="primary" key="console">
+              Go Console
+            </Button>,
+            <Button key="buy">Buy Again</Button>,
+          ]}
+        />
+      </Col>
+    </Row>
+  </div>
+);
 
 export default App;
